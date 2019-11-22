d775350f589cca5109e73cac5e3445887e940e5a
Andrew Moss <1043609+amoss@users.noreply.github.com>
2019-11-22 15:15:01 +0100

    Starting at the bottom: new re-enterant tokenizer.

--- web/server/web_client.c
+++ web/server/web_client.c
@@ -1485,6 +1485,46 @@ static inline int web_client_process_url(RRDHOST *host, struct web_client *w, ch
     return mysendfile(w, (tok && *tok)?tok:"/");
 }
 
+struct token {
+    int start;
+    int end;
+};
+
+/* To reduce complexity we tokenize without altering the original. This allows us to tokenize tokens.
+ * We cannot reallocate memory because of performance so assume we must operate on fixed size buffers
+ * (i.e. the token output). The offset parameter allows restart mid-stream to continue tokenization.
+ * On a restart we overwrite the old tokens to reuse the output buffer.
+*/
+int tokenize(struct token *out_start, size_t out_size, char *in_start, size_t size, int offset,
+             const char * const delimitors) {
+    char *in = in_start, *in_end = in_start + size;
+    struct token *out_end = (struct token *)(((char *)out_start)+out_size);
+    struct token *out = out_start;
+    int num_delims = strlen(delimitors);
+    out->start = offset;
+    while (out < out_end && in < in_end) {
+        for (int i = 0; i < num_delims; i++) {
+            if (*in == delimitors[i]) {
+                out->end = offset + in - in_start - 1;      // Zero length tokens have end<start, ranges are inclusive
+                out++;
+                if (out<out_end)
+                    out->start = offset + in - in_start;
+                break;
+            }
+        }
+        in++;
+    }
+    return out - out_start;
+}
+
+/*
+void web_client_process_request(struct web_client *w) {
+    int n = tokenize(&tokens, sizeof(tokens), w->data.buffer, w->data.len, "\n");
+
+}
+*/
+
+
 void web_client_process_request(struct web_client *w) {
 
     // start timing us
