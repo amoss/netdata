63a13cf6c8a21668fe9bf2d081ddd182c2833d43
Andrew Moss <1043609+amoss@users.noreply.github.com>
2019-11-26 09:41:57 +0100

    Fixed the use of offset inside the tokenizer.

--- libnetdata/string/tokens.c
+++ libnetdata/string/tokens.c
@@ -15,7 +15,7 @@
 */
 int tokenize(struct token *out_start, size_t out_size, const char *in_start, size_t size, int offset,
              const char * const delimitors) {
-    char *in = in_start, *in_end = in_start + size;
+    char *in = in_start + offset, *in_end = in_start + size;
     struct token *out_end = (struct token *)(((char *)out_start) + out_size);
     struct token *out = out_start;
     int num_delims = strlen(delimitors);
@@ -23,10 +23,10 @@ int tokenize(struct token *out_start, size_t out_size, const char *in_start, siz
     while (out < out_end && in < in_end) {
         for (int i = 0; i < num_delims; i++) {
             if (*in == delimitors[i]) {
-                out->end = offset + in - in_start - 1;      // Zero length tokens have end<start, ranges are inclusive
+                out->end = in - in_start - 1;      // Zero length tokens have end<start, ranges are inclusive
                 out++;
                 if (out < out_end)
-                    out->start = offset + in - in_start + 1;
+                    out->start = in - in_start + 1;
                 else
                     return out - out_start;
                 break;
@@ -37,7 +37,7 @@ int tokenize(struct token *out_start, size_t out_size, const char *in_start, siz
     // Do we emit a final token or leave it for next time?
     // Note that out<out_end to exit the loop above.
     if (*in == 0 ) {
-        out->end = offset + in - in_start - 1;
+        out->end = in - in_start - 1;
         out++;
     }
     return out - out_start;
